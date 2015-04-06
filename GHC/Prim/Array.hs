41032c8b61661583bb2126bcf1820823db1b4c4b
András Kovács <puttamalac@gmail.com>
2015-04-06 08:44:50 +0200

    add some documentation, rename modules

--- GHC/Prim/Array.hs
+++ GHC/Prim/Array.hs
@@ -7,22 +7,18 @@ module GHC.Prim.Array where
 
 import GHC.Prim (Int#, Array#, Any)
 
+-- | Prepend an element to an array.
 foreign import prim "consArray" consArray#
   :: Any -> Array# a -> Array# a
 
+-- | Append an element to the end of an array.
 foreign import prim "snocArray" snocArray#
   :: Array# a -> Any -> Array# a
 
+-- | Insert new element at position.
 foreign import prim "insertArray" insertArray#
   :: Int# -> Any -> Array# a -> Array# a
 
+-- | Delete element at position. 
 foreign import prim "deleteArray" deleteArray#
   :: Int# -> Array# a -> Array# a
-
-
-
-
-
-
-
-
--- GHC/Prim/SmallArray.hs
+++ GHC/Prim/SmallArray.hs
@@ -7,22 +7,18 @@ module GHC.Prim.SmallArray where
 
 import GHC.Prim (Int#, SmallArray#, Any)
 
+-- | Prepend an element to an array.
 foreign import prim "consSmallArray" consSmallArray#
   :: Any -> SmallArray# a -> SmallArray# a
 
+-- | Append an element to the end of an array.
 foreign import prim "snocSmallArray" snocSmallArray#
   :: SmallArray# a -> Any -> SmallArray# a
 
+-- | Insert new element at position.
 foreign import prim "insertSmallArray" insertSmallArray#
   :: Int# -> Any -> SmallArray# a -> SmallArray# a
 
+-- | Delete element at position. 
 foreign import prim "deleteSmallArray" deleteSmallArray#
   :: Int# -> SmallArray# a -> SmallArray# a
-
-
-
-
-
-
-
-
